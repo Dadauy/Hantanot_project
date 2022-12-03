298338894ae49910f10b657dab88680aaddcbc40
CLDIKE <kostya.isakov.05@mail.ru>
2022-12-03 23:46:15 +0500

    Доделал второй пункт

--- interface/user_interface.py
+++ interface/user_interface.py
@@ -4,6 +4,9 @@ from messages import default_messages_user, keyboards_user
 from database import db_session
 from database.party import Party
 from database.programma import Programma
+from database.organizators import Moder
+from database.speakers import Speaker
+from database.temas import Tema
 
 
 def user(bot: telebot.TeleBot, message, db_sess):
@@ -60,6 +63,7 @@ def user(bot: telebot.TeleBot, message, db_sess):
                                reply_markup=keyboards_user.get_mainkb())
         bot.register_next_step_handler(msg, choice)
 
+    # Даёт расписание на день
     @bot.callback_query_handler(func=lambda call: call.data.startswith("day_num"))
     def select_day(call):
         day_num = int(call.data.replace("day_num", ""))
@@ -73,4 +77,62 @@ def user(bot: telebot.TeleBot, message, db_sess):
         bot.send_message(call.message.chat.id, "Вот все мероприятия на этот день!",
                          reply_markup=keyboards_user.get_go_to_main_menukb())
 
+    # Даёт список Модераторов
+    @bot.callback_query_handler(func=lambda call: call.data.startswith("moder_num"))
+    def select_moder(call):
+        id_ivent = int(call.data.replace("moder_num", ""))
+        moderators = db_sess.query(Moder).filter(Moder.id_party == id_ivent).all()
+        bot.send_message(call.message.chat.id, "Список орагнизаторов:")
+        for moder in moderators:
+            des = moder.name
+            keyb = telebot.types.InlineKeyboardMarkup()
+            data = 'current_moder_id' + str(moder.id)
+            keyb.add(telebot.types.InlineKeyboardButton(text="Узнать больше", callback_data=data))
 
+            bot.send_message(call.message.chat.id, des, reply_markup=keyb)
+        bot.send_message(message.chat.id, "Это все организаторы!", reply_markup=keyboards_user.get_go_to_main_menukb())
+
+    # Даёт список Спикеров
+    @bot.callback_query_handler(func=lambda call: call.data.startswith("speaker_num"))
+    def select_moder(call):
+        id_ivent = int(call.data.replace("speaker_num", ""))
+        speakers = db_sess.query(Speaker).filter(Speaker.id_party == id_ivent).all()
+        bot.send_message(call.message.chat.id, "Список спикеров:")
+        for speaker in speakers:
+            des = speaker.name
+            keyb = telebot.types.InlineKeyboardMarkup()
+            data = 'current_speaker_id' + str(speaker.id)
+            keyb.add(telebot.types.InlineKeyboardButton(text="Узнать больше", callback_data=data))
+
+            bot.send_message(call.message.chat.id, des, reply_markup=keyb)
+        bot.send_message(message.chat.id, "Это все спикеры!", reply_markup=keyboards_user.get_go_to_main_menukb())
+
+    # Даёт список тем
+    @bot.callback_query_handler(func=lambda call: call.data.startswith("obs_num"))
+    def select_moder(call):
+        id_ivent = int(call.data.replace("obs_num", ""))
+        temas = db_sess.query(Tema).filter(Tema.id_party == id_ivent).all()
+        bot.send_message(call.message.chat.id, "Список тем:")
+        msg = ""
+        for tema in temas:
+            msg += tema.comment + "\n"
+        bot.send_message(call.message.chat.id, msg, reply_markup=keyboards_user.get_go_to_main_menukb())
+    # Информация о конкретном спикере
+    @bot.callback_query_handler(func=lambda call: call.data.startswith("current_speaker_id"))
+    def select_moder(call):
+        id_speaker = int(call.data.replace("current_speaker_id", ""))
+        speaker = db_sess.query(Speaker).filter(Speaker.id == id_speaker)
+
+        des = "Имя: " + speaker.name + "\n"
+        des += "Немного о спикере: " + speaker.comment
+        bot.send_message(call.message.chat.id, des, reply_markup=keyboards_user.get_go_to_main_menukb())
+
+        # Информация о конкретном модере
+        @bot.callback_query_handler(func=lambda call: call.data.startswith("current_moder_id"))
+        def select_moder(call):
+            id_moder = int(call.data.replace("current_moder_id", ""))
+            moder = db_sess.query(Moder).filter(Moder.id == id_moder)
+
+            des = "Имя: " + moder.name + "\n"
+            des += "Немного о спикере: " + moder.comment
+            bot.send_message(call.message.chat.id, des, reply_markup=keyboards_user.get_go_to_main_menukb())
