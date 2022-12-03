ab790a7892e96085dfd324155d1f50bfd6e4e067
CLDIKE <kostya.isakov.05@mail.ru>
2022-12-03 09:09:38 +0500

    create default_messages start command

--- kosti.py
+++ kosti.py
@@ -1,4 +1,16 @@
+import default_messages
+from config import BOT_TOKEN
 import telebot
 
-def f():
-    pass
+# init
+bot = telebot.TeleBot(BOT_TOKEN)
+#
+
+@bot.message_handler(commands=['start'])
+def start_hello(message):
+    bot.send_message(message.from_user.id, default_messages.HELLO_MESSAGE)
+
+
+
+
+bot.infinity_polling()
